350ea8b03c73cf9c1ed5a8bcceb0a590219c34e8
Masaki Saito <msaito@preferred.jp>
2023-07-07 02:03:53 +0900

    Fix

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='zunda',
-    version='0.0.1',
+    version='0.1',
     author='Masaki Saito',
     author_email='msaito@preferred.jp',
-    description='A simple python package for creating simple zunda videos',
+    description='A simple python package for creating zunda-mon videos',
     packages=find_packages(),
     include_package_data=True,
     package_data={
@@ -31,14 +31,15 @@ setup(
         ],
     },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
