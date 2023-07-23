daa48ac6f97d484db40a680cf80b6f14c78d8725
Masaki Saito <msaito@preferred.jp>
2023-07-23 21:54:47 +0900

    WIP

--- zunda/__init__.py
+++ zunda/__init__.py
@@ -1,16 +1,16 @@
-from zunda.audio import ( # noqa
-    concat_audio_files,
-    make_loop_music,
-)
-from zunda.motion import Motion # noqa
 from zunda.action import ( # noqa
     Action,
     parse_action_command,
     make_action_functions,
 )
+from zunda.audio import ( # noqa
+    concat_audio_files,
+    make_loop_music,
+)
 from zunda.layer import ( # noqa
     Composition, Layer, ImageLayer, VideoLayer, SlideLayer, CharacterLayer,
 )
+from zunda.motion import Motion # noqa
 from zunda.subtitle import make_ass_file # noqa
 from zunda.transform import ( # noqa
     Transform,
