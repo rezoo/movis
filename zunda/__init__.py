01c3aeff9e5373dbe2c0b962357bf044ed058f64
Masaki Saito <msaito@preferred.jp>
2023-07-28 16:42:28 +0900

    Reformat

--- zunda/__init__.py
+++ zunda/__init__.py
@@ -1,11 +1,12 @@
 from zunda.action import (Action, make_action_functions,  # noqa
                           parse_action_command)
 from zunda.audio import concat_audio_files, make_loop_music  # noqa
-from zunda.layer import (CharacterLayer, Composition, ImageLayer,  # noqa
-                         Layer, LayerProperty, SlideLayer, VideoLayer)
+from zunda.layer import ImageLayer  # noqa; noqa
+from zunda.layer import (CharacterLayer, Composition, Layer, LayerProperty, # noqa
+                         SlideLayer, VideoLayer)
 from zunda.motion import Motion  # noqa
 from zunda.subtitle import make_ass_file  # noqa
 from zunda.transform import Transform, alpha_composite, resize  # noqa
-from zunda.utils import (add_materials_to_video,  # noqa
-                         make_timeline_from_voicevox, make_voicevox_dataframe,
+from zunda.utils import add_materials_to_video  # noqa; noqa
+from zunda.utils import (make_timeline_from_voicevox, make_voicevox_dataframe, # noqa
                          merge_timeline)
