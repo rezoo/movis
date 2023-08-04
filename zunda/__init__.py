5dd79dd41250131fcf5c6c07755cabafc3c9bf75
Masaki Saito <msaito@preferred.jp>
2023-08-04 22:11:00 +0900

    Rename

--- zunda/__init__.py
+++ zunda/__init__.py
@@ -7,6 +7,6 @@ from .layer.media import (CharacterLayer, ImageLayer, SlideLayer,  # noqa
 from .motion import Motion  # noqa
 from .subtitle import make_ass_file  # noqa
 from .transform import Transform  # noqa
-from .utils import add_materials_to_video  # noqa
-from .utils import make_timeline_from_voicevox  # noqa
-from .utils import make_voicevox_dataframe, merge_timeline  # noqa
+from .util import add_materials_to_video  # noqa
+from .util import make_timeline_from_voicevox  # noqa
+from .util import make_voicevox_dataframe, merge_timeline  # noqa
