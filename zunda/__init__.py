7826d1f6a0e3df54a662e5e9d4a4177055327286
Masaki Saito <msaito@preferred.jp>
2023-08-04 21:42:10 +0900

    Add layer directory

--- zunda/__init__.py
+++ zunda/__init__.py
@@ -1,12 +1,13 @@
-from zunda.action import (Action, make_action_functions,  # noqa
-                          parse_action_command)
-from zunda.audio import concat_audio_files, make_loop_music  # noqa
-from zunda.layer import ImageLayer  # noqa
-from zunda.layer import (CharacterLayer, Composition, Layer, LayerProperty, # noqa
-                         SlideLayer, VideoLayer)
-from zunda.motion import Motion  # noqa
-from zunda.subtitle import make_ass_file  # noqa
-from zunda.transform import Transform, alpha_composite, resize  # noqa
-from zunda.utils import add_materials_to_video  # noqa
-from zunda.utils import make_timeline_from_voicevox  # noqa
-from zunda.utils import make_voicevox_dataframe, merge_timeline # noqa
+from .action import Action, make_action_functions  # noqa
+
+from .audio import concat_audio_files, make_loop_music  # noqa
+from .layer.composition import Composition, LayerItem  # noqa
+from .layer.core import Layer  # noqa
+
+from .motion import Motion  # noqa
+from .subtitle import make_ass_file  # noqa
+from .transform import Transform, alpha_composite, resize  # noqa
+
+from .utils import add_materials_to_video  # noqa
+from .utils import make_timeline_from_voicevox  # noqa
+from .utils import make_voicevox_dataframe, merge_timeline # noqa
---
+++ zunda/layer/core.py
@@ -0,0 +1,37 @@
+from typing import Hashable, Optional, Protocol, Sequence
+
+import numpy as np
+
+
+class Layer(Protocol):
+    @property
+    def duration(self):
+        raise NotImplementedError
+
+    def get_key(self, time: float) -> Hashable:
+        raise NotImplementedError
+
+    def __call__(self, time: float) -> Optional[np.ndarray]:
+        raise NotImplementedError
+
+
+class TimelineMixin:
+    def __init__(
+        self, start_times: Sequence[float], end_times: Sequence[float]
+    ) -> None:
+        assert len(start_times) == len(
+            end_times
+        ), f"{len(start_times)} != {len(end_times)}"
+        self.start_times: np.ndarray = np.asarray(start_times, dtype=float)
+        self.end_times: np.ndarray = np.asarray(end_times, dtype=float)
+
+    def get_state(self, time: float) -> int:
+        idx = self.start_times.searchsorted(time, side="right") - 1
+        if idx >= 0 and self.end_times[idx] > time:
+            return int(idx)
+        else:
+            return -1
+
+    @property
+    def duration(self):
+        return self.end_times[-1] - self.start_times[0]
