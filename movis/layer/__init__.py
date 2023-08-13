19a160b1feff46d7c96c275a74c84a6994c11709
Masaki Saito <msaito@preferred.jp>
2023-08-14 01:28:49 +0900

    Add Ellipse

--- movis/layer/__init__.py
+++ movis/layer/__init__.py
@@ -1,6 +1,7 @@
 from .component import Component  # noqa
 from .composition import Component, Composition, concatenate  # noqa
-from .drawing import FillProperty, Rectangle, StrokeProperty, Text  # noqa
+from .drawing import (Ellipse, FillProperty, Rectangle, StrokeProperty,  # noqa
+                      Text)
 from .layer import Layer  # noqa
 from .media import Character, Image, Slide, Video  # noqa
 from .mixin import TimelineMixin  # noqa
