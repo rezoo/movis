fe0211802fd2e46e45ca3e0e5004d0f012708056
Masaki Saito <msaito@preferred.jp>
2023-08-16 21:22:14 +0900

    Add GaussianBlur

--- movis/effect/blur.py
+++ movis/effect/blur.py
@@ -10,9 +10,17 @@ class GaussianBlur(AttributesMixin):
         self.radius = Attribute(radius, AttributeType.SCALAR)
 
     def __call__(self, prev_image: np.ndarray, time: float) -> np.ndarray:
-        def get_ksize(sigma):
-            return 2 * int(np.ceil(max(1, (3 * sigma - 1) / 2))) + 1
+        assert prev_image.ndim == 3
+        assert prev_image.shape[2] == 4, f'prev_image must be RGBA image, but {prev_image.shape}'
         radius = float(self.radius(time))
-        assert 0 < radius, f'radius must be positive, but {radius}'
-        ksize = get_ksize(radius)
-        return cv2.GaussianBlur(src=prev_image, ksize=ksize, sigmaX=radius, sigmaY=radius)
+        if radius == 0:
+            return prev_image
+        assert 0 < radius, f'radius must be nonnegative, but {radius}'
+
+        k = 2 * int(np.ceil(max(1, (4 * radius - 1) / 2))) + 1
+        ksize = (k, k)
+        rgb_image = np.pad(prev_image[:, :, :3], (ksize, ksize, (0, 0)), mode='edge')
+        alpha_image = np.pad(prev_image[:, :, 3:], (ksize, ksize, (0, 0)), mode='constant')
+        return cv2.GaussianBlur(
+            src=np.concatenate([rgb_image, alpha_image], axis=2),
+            ksize=ksize, sigmaX=radius, sigmaY=radius)
