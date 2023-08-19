e09b10dd2a935b8ed5e0f744c6878a128bafa167
Masaki Saito <msaito@preferred.jp>
2023-08-19 22:57:51 +0900

    Add property

--- movis/attribute.py
+++ movis/attribute.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Hashable, Optional, Sequence, Union
+from typing import Callable, Hashable, Optional, Sequence, Union
 
 import numpy as np
 
@@ -83,13 +83,16 @@ class Attribute:
         init_value: Union[float, tuple[float, ...], np.ndarray],
         value_type: AttributeType,
         range: Optional[tuple[float, float]] = None,
+        motion: Optional[Motion] = None,
+        function: Optional[Callable[[np.ndarray, float], np.ndarray]] = None,
     ) -> None:
         np_value = transform_to_numpy(init_value, value_type)
         clipped_value = np.clip(np_value, range[0], range[1]) if range is not None else np_value
         self.init_value: np.ndarray = clipped_value
         self.value_type = value_type
         self.range = range
-        self._motion: Optional[Motion] = None
+        self._motion: Optional[Motion] = motion
+        self._function: Optional[Callable[[np.ndarray, float], np.ndarray]] = function
 
     def __call__(self, layer_time: float) -> np.ndarray:
         if self._motion is None:
@@ -98,10 +101,13 @@ class Attribute:
             value = self.init_value
             if self._motion is not None:
                 value = self._motion(value, layer_time)
+            if self._function is not None:
+                value = self._function(value, layer_time)
             np_value = transform_to_numpy(value, self.value_type)
-            clipped_value = np.clip(np_value, self.range[0], self.range[1]) \
-                if self.range is not None else np_value
-            return clipped_value
+            if self.range is not None:
+                return np.clip(np_value, self.range[0], self.range[1])
+            else:
+                return np_value
 
     def enable_motion(self) -> Motion:
         if self._motion is None:
@@ -111,6 +117,25 @@ class Attribute:
 
     def disable_motion(self) -> None:
         self._motion = None
+
+    @property
+    def motion(self) -> Optional[Motion]:
+        return self._motion
+
+    def enable_function(
+        self, function: Callable[[np.ndarray, float], np.ndarray]
+    ) -> Callable[[np.ndarray, float], np.ndarray]:
+        if not callable(function):
+            raise ValueError(f"Invalid function: {function}")
+        self._function = function
+        return function
+
+    def disable_function(self) -> None:
+        self._function = None
+
+    @property
+    def function(self) -> Optional[Callable[[np.ndarray, float], np.ndarray]]:
+        return self._function
 
     def __repr__(self) -> str:
         if self._motion is None:
