56408e1ae099c8c89cba330f7c6ded6ea7d97c48
Masaki Saito <msaito@preferred.jp>
2023-08-25 13:38:59 +0900

    Add tests

---
+++ tests/test_imgproc.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+import pytest
+from movis.imgproc import alpha_composite
+from movis.enum import BlendingMode
+
+
+alpha_composite_params = [
+    (0.5, BlendingMode.NORMAL),
+    (1.0, BlendingMode.ADD),
+    (0.3, BlendingMode.HARD_LIGHT),
+    (0.2, BlendingMode.MULTIPLY),
+    (0.9, BlendingMode.OVERLAY),
+    (1.0, BlendingMode.SCREEN),
+    (0.1, BlendingMode.SOFT_LIGHT),
+]
+
+
+@pytest.mark.parametrize("opacity, blending_mode", alpha_composite_params)
+def test_alpha_composite(opacity, blending_mode):
+    bg = np.random.randint(0, 255, size=(128, 256, 4)).astype(np.uint8)
+    fg = np.random.randint(0, 255, size=(64, 128, 4)).astype(np.uint8)
+
+    for (x, y) in [(0, 0), (-10, -20), (96, 48), (-10, 48), (96, -20)]:
+        bg_dst = alpha_composite(
+            bg.copy(), fg, position=(x, y),
+            opacity=opacity, blending_mode=blending_mode)
+        assert bg.shape == bg_dst.shape
+        assert bg_dst.dtype == np.uint8
+
+    for (x, y) in [(-200, -200), (512, 512)]:
+        bg_dst = alpha_composite(
+            bg.copy(), fg, position=(x, y),
+            opacity=opacity, blending_mode=blending_mode)
+        np.testing.assert_allclose(bg, bg_dst)
+        assert bg_dst.dtype == np.uint8
+
+    for (x, y) in [(0, 0), (-10, -20), (96, 48), (-10, 48), (96, -20)]:
+        bg_dst = alpha_composite(
+            bg.copy(), fg, position=(x, y),
+            opacity=opacity, blending_mode=blending_mode, alpha_matte_mode=True)
+        np.testing.assert_allclose(bg[:, :, 3], bg_dst[:, :, 3])
+        assert bg_dst.dtype == np.uint8
