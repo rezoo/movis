454bea61d6c49c4e62f1b4fa72457ea67ce8a8d8
Masaki Saito <msaito@preferred.jp>
2023-09-07 17:51:39 +0900

    Update

--- examples/application/zundamon_commentary/render.py
+++ examples/application/zundamon_commentary/render.py
@@ -6,6 +6,29 @@ from movis.contrib.commentary import Character, Slide
 from movis.contrib.voicevox import make_voicevox_dataframe
 
 
+def circle_accent_explosion(
+        in_time: float = 0.3, out_time: float = 0.3, n_line=12,
+        line_range: tuple[float, float] = (100., 200.,),
+        line_width: float = 5.0, line_color: str = "#ffffff", eps: float = 10.0):
+    duration = in_time + out_time
+
+    L = int(np.ceil(2 * (line_range[1] + eps)))
+    size = (L, L)
+
+    scene = mv.layer.Composition(size, duration=duration)
+    theta = np.linspace(0, 2 * np.pi, n_line, endpoint=False)
+    center = np.array([L / 2, L / 2])
+    for u in theta:
+        p0 = line_range[0] * np.array([np.cos(u), np.sin(u)]) + center
+        p1 = line_range[1] * np.array([np.cos(u), np.sin(u)]) + center
+        line = mv.layer.Line(
+            size, start=p0, end=p1, duration=duration, width=line_width, color=line_color)
+        line.trim_end.enable_motion().extend([0, in_time], [0, 1], ['ease_in_out3'])
+        line.trim_start.enable_motion().extend([in_time, duration], [0, 1], ['ease_in_out3'])
+        scene.add_layer(line)
+    return scene
+
+
 def main():
     timeline = pd.read_csv('outputs/timeline.tsv', sep='\t')
     audio_timeline = make_voicevox_dataframe('audio')
@@ -39,8 +62,8 @@ def main():
     def slide_in_out(item: mv.layer.LayerItem, offset: np.ndarray):
         p = item.transform.position.init_value
         item.transform.position.enable_motion() \
-            .append(0.0, p + offset, 'ease_out5') \
-            .append(1.0, p).append(item.duration - 1.0, p, 'ease_in5') \
+            .append(0.0, p + offset, 'ease_out10') \
+            .append(1.0, p).append(item.duration - 1.0, p, 'ease_in10') \
             .append(item.duration, p + offset)
         item.transform.opacity.enable_motion().extend(
             keyframes=[0, 1, item.duration - 1.0, item.duration], values=[0, 1, 1, 0],
@@ -76,6 +99,12 @@ def main():
             mv.layer.Image(img_file='assets/logo_metan.png', duration=6.0),
             name='metan_logo', offset=0.5, position=(170, 340)),
         np.array([-500, 0]))
+    scene.add_layer(
+        circle_accent_explosion(line_range=(75, 150), line_color='#99BF4D', line_width=5.0),
+        position=(1755, 340), offset=0.75)
+    scene.add_layer(
+        circle_accent_explosion(line_range=(75, 150), line_color='#D968B2', line_width=5.0),
+        position=(170, 340), offset=0.75)
 
     sections = tl[~tl['section'].isnull()]
     section_times = np.concatenate([sections['start_time'].to_numpy(), [3600.0]])
