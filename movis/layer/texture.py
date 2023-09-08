9bf9174d3d39c00d7cc888ccecae8ded2b4d3fb2
Masaki Saito <msaito@preferred.jp>
2023-09-08 19:42:46 +0900

    Add docs

--- movis/layer/texture.py
+++ movis/layer/texture.py
@@ -90,6 +90,51 @@ class Gradient(AttributesMixin):
 
 
 class Stripe(AttributesMixin):
+    """A layer that generates a stripe pattern.
+
+    Args:
+        size:
+            the size of the generated image. Defaults to ``(100, 100)``.
+        angle:
+            the angle of the stripe pattern in degrees. Defaults to ``45.0``.
+        color1:
+            the first color of the stripe pattern. Defaults to ``(0, 0, 0)``.
+            the color can be specified as a tuple of ``(R, G, B)``,
+            or a string (`e.g.,` ``"#ff0000"``, or ``"red"``).
+        color2:
+            the second color of the stripe pattern. Defaults to ``(255, 255, 255)``.
+        opacity1:
+            the opacity of the first color. Defaults to ``1.0``.
+        opacity2:
+            the opacity of the second color. Defaults to ``1.0``.
+        total_width:
+            the total width of the stripe pattern. The stripe pattern repeats every ``total_width``.
+            Defaults to ``64.0``.
+        phase:
+            the phase of the stripe pattern in degrees. Defaults to ``0.0``.
+        ratio:
+            the ratio of the first color. Defaults to ``0.5``.
+        duration:
+            the duration of the layer. Defaults to ``1e6``.
+
+    Animatable Attributes:
+        angle:
+            the angle of the stripe pattern in degrees.
+        color1:
+            the first color of the stripe pattern.
+        color2:
+            the second color of the stripe pattern.
+        opacity1:
+            the opacity of the first color.
+        opacity2:
+            the opacity of the second color.
+        total_width:
+            the total width of the stripe pattern.
+        phase:
+            the phase of the stripe pattern in degrees.
+        ratio:
+            the ratio of the first color.
+    """
     def __init__(
         self,
         size: tuple[int, int] = (100, 100),
