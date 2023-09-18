63a81700b6414b8babf5c310d82ebc6fabe5f51b
Masaki Saito <msaito@preferred.jp>
2023-09-18 14:01:39 +0900

    Update theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ author = 'Masaki Saito'
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx_copybutton",
 ]
 
 templates_path = ['_templates']
@@ -28,7 +29,7 @@ autosummary_generate = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 
 intersphinx_mapping = {
