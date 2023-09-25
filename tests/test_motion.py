1beb0499cec98497c3af6930920862a94f652628
Masaki Saito <msaito@preferred.jp>
2023-09-25 17:19:49 +0900

    Add tests for easing

--- tests/test_motion.py
+++ tests/test_motion.py
@@ -3,6 +3,7 @@ import pytest
 
 from movis.attribute import AttributeType
 from movis.motion import Motion
+from movis import Easing
 
 
 def test_motion_call():
@@ -131,3 +132,25 @@ def test_motion_clear(value_type, n_dim):
     motion.extend(keyframes=[0.0, 0.5, 1.0], values=[1.0, 1.0, 2.0])
     motion.clear()
     assert np.all(motion(None, -1.0) == np.array([0.0] * n_dim))
+
+
+@pytest.mark.parametrize("value_type, n_dim", motion_append_params)
+def test_motion_easing(value_type, n_dim):
+    motion = Motion(init_value=[0.0] * n_dim, value_type=value_type)
+    motion.extend(keyframes=[0.0, 1.0], values=[0.0, 1.0], easings=[Easing.LINEAR])
+
+    assert np.all(motion(None, 0.0) == np.array([0.0] * n_dim))
+    assert np.all(motion(None, 1.0) == np.array([1.0] * n_dim))
+
+    motion = Motion(init_value=[0.0] * n_dim, value_type=value_type)
+    motion.extend(keyframes=[0.0, 1.0], values=[0.0, 1.0], easings=['linear'])
+
+    assert np.all(motion(None, 0.0) == np.array([0.0] * n_dim))
+    assert np.all(motion(None, 1.0) == np.array([1.0] * n_dim))
+
+    motion = Motion(init_value=[0.0] * n_dim, value_type=value_type)
+    motion.extend(keyframes=[0.0, 1.0], values=[0.0, 1.0], easings=[Easing.EASE_IN_OUT])
+
+    assert np.all(motion(None, 0.0) == np.array([0.0] * n_dim))
+    assert np.all(motion(None, 0.5) == np.array([0.5] * n_dim))
+    assert np.all(motion(None, 1.0) == np.array([1.0] * n_dim))
