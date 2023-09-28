f82b990414560a0096fd2ad5afed68246e04d23c
Masaki Saito <msaito@preferred.jp>
2023-09-28 14:18:28 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,24 @@ def parse_requirements(name: str = "requirements.txt"):
         return f.read().splitlines()
 
 
+with open(BASE_DIR / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+
 setup(
     name="movis",
     version="0.6",
     author="Masaki Saito",
     author_email="msaito@preferred.jp",
     description="A video editing library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="video video-processing video-editing",
+    url="https://github.com/rezoo/movis",
+    project_urls={
+        "Source": "https://github.com/rezoo/movis",
+    },
+    license="MIT License",
     packages=find_packages(),
     include_package_data=True,
     package_data={
