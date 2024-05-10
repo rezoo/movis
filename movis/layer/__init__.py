b0ee2326c9eb9dac60ee2d6df072d26a1bb2185b
Jingcheng Yang <JingchengYang4@icloud.com>
2024-05-10 23:45:31 +0800

    removed video frame sequence

--- movis/layer/__init__.py
+++ movis/layer/__init__.py
@@ -2,7 +2,7 @@ from .composition import Composition, LayerItem  # noqa
 from .drawing import (Ellipse, FillProperty, Line, Rectangle,  # noqa
                       StrokeProperty, Text)
 from .layer_ops import AlphaMatte, LuminanceMatte  # noqa
-from .media import Audio, AudioSequence, Image, ImageSequence, Video, VideoFrameSequence  # noqa
+from .media import Audio, AudioSequence, Image, ImageSequence, Video  # noqa
 from .mixin import TimelineMixin  # noqa
 from .protocol import BasicLayer, Layer  # noqa
 from .texture import Gradient, Stripe  # noqa
